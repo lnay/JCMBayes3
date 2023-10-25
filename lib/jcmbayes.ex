040ba8d9c2f3f8e183de17774d1dca63f835c2a7
lnay <luke@grahamnaylor.net>
2023-10-25 23:54:18 +0100

    BROKEN: copy example from nostrum README

--- lib/jcmbayes.ex
+++ lib/jcmbayes.ex
@@ -1,4 +1,5 @@
-defmodule Jcmbayes do
+defmodule Jcmbayes.Application do
+  use Application
   @moduledoc """
   Documentation for `Jcmbayes`.
   """
@@ -12,7 +13,44 @@ defmodule Jcmbayes do
       :world
 
   """
+  def start(_type, _args) do
+    children = [MessageConsumer]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
   def hello do
     :world
+  end
+end
+
+
+defmodule MessageConsumer do
+  use Nostrum.Consumer
+
+  alias Nostrum.Api
+
+  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+    case msg.content do
+      "!sleep" ->
+        Api.create_message(msg.channel_id, "Going to sleep...")
+        # This won't stop other events from being handled.
+        Process.sleep(3000)
+
+      "!ping" ->
+        Api.create_message(msg.channel_id, "pyongyang!")
+
+      "!raise" ->
+        # This won't crash the entire Consumer.
+        raise "No problems here!"
+
+      _ ->
+        :ignore
+    end
+  end
+
+  # Default event handler, if you don't include this, your consumer WILL crash if
+  # you don't have a method definition for each event type.
+  def handle_event(_event) do
+    :noop
   end
 end
