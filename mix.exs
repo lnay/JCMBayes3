fe1266c4c8fcaeede68fc570eecce453bbc81859
lnay <luke@grahamnaylor.net>
2023-11-17 00:50:30 +0000

    Start a supervisor from the application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Jcmbayes.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Jcmbayes, []},
+      mod: {Jcmbayes.Application, []},
       extra_applications: [:logger]
     ]
   end
