c6bcd4a860ddfdb393f581c1dd750bf8b8c9035a
lnay <luke@grahamnaylor.net>
2023-11-17 00:40:13 +0000

    Move discord bot intents out of secret config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,11 @@
 import Config
 
+config :nostrum,
+  gateway_intents: [
+    :guild_messages,
+    :guild_message_reactions,
+    :message_content
+  ]
 # config :logger, :console, metadata: [:shard, :guild, :channel]
 
 if File.exists?("config/secret.exs"), do: import_config("secret.exs")
